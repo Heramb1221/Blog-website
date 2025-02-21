a38032a0409a3badfd37694a997b3e7db40add28
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-02-21 13:50:34 +0530

    Blog Website

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='w-full border shadow-lg py-4 fixed top-0 bg-white'>
+        <header className='text-center' >
+            <h1 className='text-3xl font-extrabold uppercase'>Blogs</h1>
+        </header>
+    </div>
+  )
+}
---
+++ src/Components/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.css'
+
+const Spinner = () => {
+  return (
+    <div className='h-screen items-center'>
+        <div className='spinner'></div>
+    </div>
+  )
+}
+
+export default Spinner
